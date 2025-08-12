6f01b56cd34b075bace41d026b93f10e1236f714
Shankar Elavarasan <shankar@example.com>
2025-08-12 23:36:18 +0530

    Deploy correct Rapid Mixer UI

--- rapid mixer 1.0/temp-frontend/flutter_service_worker.js
+++ rapid mixer 1.0/temp-frontend/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f476448a54c88567298a115f20a11147
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "07e9b7899bfce9d6c5a1d59b689d11b7",
+"flutter_bootstrap.js": "b9d5161bb69c1de0306c45d03cfa75c3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
